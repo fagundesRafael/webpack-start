3b38f414d0c1fccad0003380d1b449bc2a4dcf84
Rafael Fagundes <fagundesrafael88@gmail.com>
2022-06-18 21:22:07 -0400

    Learningand testing webpack configuration and possibilities.

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path")
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+        galaxy: './src/galaxy.js'
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    module: {
+        rules: [
+            {
+                test:/\.js$/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+        allowedHosts: 'all'
+      },
+    
+
+  };
